c64a1b310fa2d170380b959039ff57f3fd3ca7df
Gian Bernardo <gianszabernardo@gmail.com>
2019-12-29 22:59:47 -0300

    Setup the app index

--- src/index.js
+++ src/index.js
@@ -1,10 +1,27 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+// React.
+import React from "react";
+import ReactDOM from "react-dom";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// Redux.
+import { Provider } from "react-redux";
+import store from "./store";
+
+// Custom Containers.
+import App from "./containers/App";
+
+// Styles.
+import GlobalStyle from "./utils/globals";
+
+// Service Workers.
+import * as serviceWorker from "./serviceWorker";
+
+ReactDOM.render(
+  <Provider store={store}>
+    <GlobalStyle />
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
